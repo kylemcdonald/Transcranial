a4bf5b3e3f4f1ac8806f11b0a14d430ce34c29a0
Kyle McDonald <kyle@kylemcdonald.net>
2014-03-25 13:38:42 +0100

    blending with FBOs instead, going to move to ofxF

--- Blending/bin/data/blend.frag
+++ Blending/bin/data/blend.frag
@@ -1,12 +1,11 @@
 #version 120
 
 uniform sampler2DRect bg, fg;
-uniform vec2 offset;
 varying vec2 texCoord;
 
 void main() {
     vec3 bgColor = texture2DRect(bg, texCoord).rgb;
-    vec3 fgColor = texture2DRect(fg, texCoord - offset).rgb;
+    vec3 fgColor = texture2DRect(fg, texCoord).rgb;
     float bgBright = length(bgColor);
     float fgBright = length(fgColor);
     float sum = bgBright + fgBright;
--- Blending/src/main.cpp
+++ Blending/src/main.cpp
@@ -3,6 +3,7 @@
 class ofApp : public ofBaseApp {
 public:
     ofShader blend;
+    ofFbo bg, fg, blended;
     ofVideoPlayer video;
     
 	void setup() {
@@ -10,18 +11,34 @@ public:
         ofSetDataPathRoot("../../../../../SharedData/");
         video.loadMovie("milos-extreme.mov");
         video.play();
+        bg.allocate(video.getWidth(), video.getHeight());
+        fg.allocate(video.getWidth(), video.getHeight());
+        blended.allocate(video.getWidth(), video.getHeight());
 	}
 	void update() {
         video.update();
+        if(video.isFrameNew()) {
+            bg.begin();
+            ofClear(0, 0);
+            video.draw(0, 0);
+            bg.end();
+            
+            fg.begin();
+            ofClear(0, 0);
+            video.draw(mouseX, mouseY);
+            fg.end();
+            
+            blended.begin();
+            blend.begin();
+            blend.setUniformTexture("bg", bg.getTextureReference(), 0);
+            blend.setUniformTexture("fg", fg.getTextureReference(), 1);
+            video.draw(0, 0);
+            blend.end();
+            blended.end();
+        }
 	}
 	void draw() {
-        blend.begin();
-        blend.setUniformTexture("bg", video.getTextureReference(), 0);
-        blend.setUniformTexture("fg", video.getTextureReference(), 1);
-        blend.setUniform2f("offset", mouseX, mouseY);
-        ofSetTextureWrap(GL_CLAMP, GL_CLAMP);
-        video.draw(0, 0);
-        blend.end();
+        blended.draw(0, 0);
 	}
 };
 
